55cdc3b3bd6a6fc58f8a0b5ba4b45077379dcbc5
Rafik Zebida <blue22eagle@gmail.com>
2021-09-23 15:01:56 +0100

    my forth commit

--- ExpressServer.js
+++ ExpressServer.js
@@ -2,19 +2,17 @@
 
 const 	express= require('express'),
 		app= express(),
-		url = require('url'),
-		fs = require('fs'),
-		path = require('path'),
-		cookieParser= require('cookie-parser'),
+		//path = require('path'),
+		//cookieParser= require('cookie-parser'),
 		colors= require('colors'),
 		port = process.env.PORT || process.argv[2] || 8080;
 
 //app.set('views', path.join(__dirname, 'vuejs'))
-app.set(__dirname)
+app.set('views', __dirname)
 .use(express.static(__dirname))
-.use(express.json()) // parse application/json
 .use(express.urlencoded({extended: false})) // parse application/x-www-form-urlencoded
-.use(cookieParser())
+//.use(express.json()) // parse application/json
+//.use(cookieParser())
 .get('/', (req, res)=> res.render('index.html'))
 .listen(port, () => {
 	console.log((`Server is running on http://localhost:${port}\nStart at: `+ new Date().toLocaleString()).cyan.bold);
