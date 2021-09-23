444add97b052faf8e46c4d2e4128bf08cb9a7f10
Rafik Zebida <blue22eagle@gmail.com>
2021-09-23 14:47:29 +0100

    My second commit

--- ExpressServer.js
+++ ExpressServer.js
@@ -9,12 +9,13 @@ const 	express= require('express'),
 		colors= require('colors'),
 		port = process.env.PORT || process.argv[2] || 8080;
 
-app.set('views', path.join(__dirname, 'vuejs'))
-.use(express.static(path.join(__dirname, 'vuejs')))
+//app.set('views', path.join(__dirname, 'vuejs'))
+app.set(__dirname)
+.use(express.static(__dirname, 'vuejs'))
 .use(express.json()) // parse application/json
 .use(express.urlencoded({extended: false})) // parse application/x-www-form-urlencoded
 .use(cookieParser())
-.get('/', (req, res)=> res.render('vuejs/index.html'))
+.get('/', (req, res)=> res.render('index.html'))
 .listen(port, () => {
 	console.log((`Server is running on http://localhost:${port}\nStart at: `+ new Date().toLocaleString()).cyan.bold);
 	//console.log(new Date().getTimezoneOffset() / 60);
